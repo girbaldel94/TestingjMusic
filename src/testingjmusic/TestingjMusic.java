0cf68bcef42d8b08b66a32f49f5d68375e442877
girbaldel94 <girbaldel94@gmail.com>
2016-03-21 19:28:35 +0100

    Random melody

---
+++ src/testingjmusic/TestingjMusic.java
@@ -0,0 +1,71 @@
+package testingjmusic;
+/*
+import jm.music.data.*;
+import jm.JMC;
+import jm.music.tools.*;
+import jm.util.*;
+
+public class TestingjMusic implements JMC{
+    
+    
+     // @param args the command line arguments
+     
+    public static void main(String[] args) {
+        int[] pitches = {D4, E4, FS4, G4, A4, B4, CS5, D5};
+        Phrase scale = new Phrase();
+        for (int i = 0; i < pitches.length; i++) {
+            Note n = new Note(pitches[i], QUARTER_NOTE);
+            scale.addNote(n);
+        }
+        for (int i = 1; i < pitches.length+1; i++) {
+            Note n = new Note(pitches[pitches.length-i], QUARTER_NOTE);
+            scale.addNote(n);
+        }
+        Part part = new Part();
+        part.add(scale);
+        Phrase scale2 = new Phrase();
+        for (int i = 0; i < pitches.length; i++) {
+            Note n = new Note(pitches[i], QUARTER_NOTE);
+            scale2.addNote(n);
+        }
+        for (int i = 1; i < pitches.length+1; i++) {
+            Note n = new Note(pitches[pitches.length-i], QUARTER_NOTE);
+            scale2.addNote(n);
+        }
+        part.appendPhrase(scale2);
+        Score sc = new Score(part);
+        Write.midi(sc, "CScale.mid");        
+        //View.notate(part, 20, 100);
+        View.notation(sc);
+        
+    }
+    
+}
+*/
+import jm.JMC;
+import jm.music.data.*;
+import jm.util.View;
+import jm.util.Write;
+
+/**
+ * 
+ * @author Pol Girbal
+ */
+public final class TestingjMusic implements JMC{
+	
+    public static void main(String[] args){
+        Score score = new Score("Random melody");
+        Part part = new Part("Piano", PIANO, 0);
+        Phrase phrase = new Phrase(0.0); 
+        
+        for(int i=0; i<48; i++){
+            Note note = new Note((int)(Math.random()*127), QUAVER);
+            phrase.addNote(note);
+        }
+        
+        part.addPhrase(phrase);
+        score.addPart(part);
+        Write.midi(score, "random.mid");
+        View.notation(score);
+    }
+}
